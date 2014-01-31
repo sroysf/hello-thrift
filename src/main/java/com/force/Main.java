11245459015720630d802b0d91909da064a188de
sroysf <saptarshi.roy@salesforce.com>
2014-01-30 16:24:32 -0800

    First commit

---
+++ src/main/java/com/force/Main.java
@@ -0,0 +1,13 @@
+package com.force;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main {
+
+    private static Logger logger = LoggerFactory.getLogger(Main.class);
+
+    public static void main(String[] args) {
+        logger.info("Hello world");
+    }
+}
---
+++ src/test/java/com/force/MainTest.java
@@ -0,0 +1,12 @@
+package com.force;
+
+import org.testng.annotations.Test;
+import static org.testng.Assert.*;
+
+public class MainTest {
+
+    @Test
+    public void sampleTest() {
+        assertTrue(true);
+    }
+}
